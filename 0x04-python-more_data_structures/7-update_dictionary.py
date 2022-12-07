63c947b2de56b82eb36d5d0aeb03561cb66c0a91
Ebenezer Adedotun <dotbenzer@yahoo.com>
2022-12-07 13:41:00 -0800

    solution to task 7

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    """reolace or add key/value pairs in a dictionary"""
+    a_dictionary[key] = value
+    return (a_dictionary)
